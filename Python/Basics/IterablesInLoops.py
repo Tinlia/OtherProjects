e7b3cd9c43049efabfa683fab4f201d123a7e830
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-09-30 19:59:23 -0400

    Create IterablesInLoops.py

---
+++ Python/Basics/IterablesInLoops.py
@@ -0,0 +1,12 @@
+# Iterable
+for x in "Python":
+    print(x)
+
+for y in [1,2,3,4]:
+    print(y)
+
+shopping_cart = ["Bananas", "Croissants", "Chicken", "Cherries", "Receipt"]
+for item in shopping_cart:
+    print(item)
+
+# Some complex functions are iterable, such as ranges and lists
