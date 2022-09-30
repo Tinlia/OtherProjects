2fc76ca50e52d9eebd8efffcddca9eab4185f2cd
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-09-30 18:36:51 -0400

    Create stringManipulation.py

---
+++ Python/Basics/stringManipulation.py
@@ -0,0 +1,5 @@
+course = "Python for Beginners"
+
+print(course.find('n')) # Returns the index number for the first instance of the letter in a string
+print(course.replace('n', '4')) # Replaces all instances of n with 4
+print("Python" in course) # Returns True or False if 'Python' is found or not found in the string
