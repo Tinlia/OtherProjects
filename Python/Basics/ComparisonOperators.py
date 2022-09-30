b022f1df4d9eb43c387ec9626acee65a4c41081a
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-09-30 18:48:43 -0400

    Create ComparisonOperators.py

---
+++ Python/Basics/ComparisonOperators.py
@@ -0,0 +1,17 @@
+x = 3 > 2
+print(x) # True
+
+x = 3 >= 2
+print(x) # True
+
+x = 3 < 2
+print(x) # False
+
+x = 3 <= 2
+print(x) # False
+
+x = 3 == 2
+print(x) # False
+
+x = 3 != 2
+print(x) # True
