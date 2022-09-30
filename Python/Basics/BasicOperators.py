668b00edad608982389a6bcc1a7496b0d929fc52
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-09-30 18:43:01 -0400

    Create BasicOperators.py

---
+++ Python/Basics/BasicOperators.py
@@ -0,0 +1,11 @@
+print(10 / 3)  # Divide and return a float/int
+print(10 // 3) # Divide and round to the nearest int
+print(10 % 3)  # Print the remainder of 10 / 3
+print(10 * 3)  # Print 10 x 3
+print(10 ** 3) # Print 10^3
+
+x = 10
+x = x + 3
+x += 3
+x -= 3
+x *= 3
