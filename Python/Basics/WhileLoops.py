4a6a0ed77bd442059e44f961409daf8d3e9139ee
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-12-04 14:43:23 -0500

    Update WhileLoops.py

--- Python/Basics/WhileLoops.py
+++ Python/Basics/WhileLoops.py
@@ -7,3 +7,12 @@ i = 1
 while i <= 10:
     print(i)
     i += 1
+    
+
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+while index < length:
+  print(f"I am learning about {python_topics[index]}")
+  index += 1
